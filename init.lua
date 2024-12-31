5bdfd0cb0137c38fb138ca4705cd881a64acc41d
Barinderpreet Singh <naffydharni006@gmail.com>
2025-01-01 05:27:47 +0530

    Configure auto format

--- init.lua
+++ init.lua
@@ -698,7 +698,6 @@ require('lazy').setup({
       },
     },
     opts = {
-      notify_on_error = false,
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
@@ -715,13 +714,17 @@ require('lazy').setup({
           lsp_format = lsp_format_opt,
         }
       end,
+      -- Conform will notify you when a formatter errors
+      notify_on_error = false,
+      -- Conform will notify you when no formatters are available for the buffer
+      notify_no_formatters = true,
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        ['*'] = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
