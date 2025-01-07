098728f317ef515cea1a0b82375549e1da4603f3
Barinderpreet Singh <naffydharni006@gmail.com>
2025-01-07 21:04:14 +0530

    Add command chain for trouble commands

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -51,6 +51,7 @@ return { -- Useful plugin to show you pending keybinds.
       { '<leader>s', group = '[S]earch' },
       { '<leader>w', group = '[W]orkspace' },
       { '<leader>t', group = '[T]oggle' },
+      { '<leader>x', group = 'Fi[x] Trouble', mode = { 'n', 'v' } },
       { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
     },
   },
