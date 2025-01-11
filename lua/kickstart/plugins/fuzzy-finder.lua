3a8604961f798f0f8a1e66cc06cef4a239ac872e
Barinderpreet Singh <naffydharni006@gmail.com>
2025-01-11 10:00:01 +0530

    Modify style for fuzzy search in current file

--- lua/kickstart/plugins/fuzzy-finder.lua
+++ lua/kickstart/plugins/fuzzy-finder.lua
@@ -79,14 +79,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
     vim.keymap.set('n', '<leader>sc', require('telescope.builtin').oldfiles, { desc = '[S]earch [C]losed Files' })
 
-    -- Slightly advanced example of overriding default behavior and theme
-    vim.keymap.set('n', '<leader>/', function()
-      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
-      })
-    end, { desc = '[/] Fuzzily search in current buffer' })
+    vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer' })
 
     -- It's also possible to pass additional configuration options.
     --  See `:help telescope.builtin.live_grep()` for information about particular keys
