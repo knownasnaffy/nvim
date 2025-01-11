1e7af5087bcfdc6f13268b23f5ed60ef03cb4db1
Barinderpreet Singh <naffydharni006@gmail.com>
2025-01-11 10:07:17 +0530

    Refactor

--- lua/kickstart/plugins/fuzzy-finder.lua
+++ lua/kickstart/plugins/fuzzy-finder.lua
@@ -77,7 +77,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-    vim.keymap.set('n', '<leader>sc', require('telescope.builtin').oldfiles, { desc = '[S]earch [C]losed Files' })
+    vim.keymap.set('n', '<leader>sc', builtin.oldfiles, { desc = '[S]earch [C]losed Files' })
 
     vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer' })
 
