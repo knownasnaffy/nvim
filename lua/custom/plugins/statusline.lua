b58de45914825aa63d41c81f634fb2b26479fbd4
Barinderpreet Singh <naffydharni006@gmail.com>
2025-01-11 09:55:02 +0530

    Change tabline module and location in file behaviour

--- lua/custom/plugins/statusline.lua
+++ lua/custom/plugins/statusline.lua
@@ -8,8 +8,8 @@ return { -- Statusline and Tabline
 
     require('lualine').setup {
       options = {
-        section_separators = { left = '', right = '' },
-        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '/', right = '\\' },
         theme = statusline_theme,
         disabled_filetypes = { -- Filetypes to disable lualine for.
           statusline = {
@@ -70,8 +70,9 @@ return { -- Statusline and Tabline
           },
           'diagnostics',
         },
-        lualine_y = { 'filetype' },
-        lualine_z = { { 'location', separator = { left = '', right = '' } } },
+        lualine_y = { 'progress' },
+        -- <line number>:<column number>
+        lualine_z = { '%l:%c' },
       },
       inactive_sections = {
         lualine_a = {},
@@ -80,98 +81,6 @@ return { -- Statusline and Tabline
         lualine_x = {},
         lualine_y = { 'location' },
         lualine_z = {},
-      },
-      tabline = {
-        lualine_a = {
-          {
-            'buffers',
-            separator = { left = '', right = '' },
-            right_padding = 2,
-            icons_enabled = false, -- Enables the display of icons alongside the component.
-            --   show_filename_only = true, -- Shows shortened relative path when set to false.
-            --   hide_filename_extension = false, -- Hide filename extension when set to true.
-            --   show_modified_status = true, -- Shows indicator when the buffer is modified.
-            --
-            --   mode = 0, -- 0: Shows buffer name
-            --   -- 1: Shows buffer index
-            --   -- 2: Shows buffer name + buffer index
-            --   -- 3: Shows buffer number
-            --   -- 4: Shows buffer name + buffer number
-            --
-            --   max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
-            --   -- it can also be a function that returns
-            --   -- the value of `max_length` dynamically.
-            filetype_names = {
-              TelescopePrompt = 'Telescope',
-              dashboard = 'Dashboard',
-              packer = 'Packer',
-              fzf = 'FZF',
-              alpha = 'Alpha',
-              'neo-tree' == 'Neo Tree',
-            }, -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
-            --
-            --   -- Automatically updates active buffer color to match color of other components (will be overidden if buffers_color is set)
-            --   use_mode_colors = false,
-            --
-            buffers_color = {
-              -- Same values as the general color option can be used here.
-              active = 'lualine_a_normal', -- Color for active buffer.
-              inactive = 'lualine_b_normal', -- Color for inactive buffer.
-            },
-
-            symbols = {
-              modified = ' ●', -- Text to show when the buffer is modified
-              alternate_file = '', -- Text to show to identify the alternate file
-              directory = '', -- Text to show when the buffer is a directory
-            },
-          },
-        },
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {
-          {
-            'tabs',
-            -- tab_max_length = 40, -- Maximum width of each tab. The content will be shorten dynamically (example: apple/orange -> a/orange)
-            -- max_length = vim.o.columns / 3, -- Maximum width of tabs component.
-            -- Note:
-            -- It can also be a function that returns
-            -- the value of `max_length` dynamically.
-            mode = 0, -- 0: Shows tab_nr
-            -- 1: Shows tab_name
-            -- 2: Shows tab_nr + tab_name
-
-            -- path = 0, -- 0: just shows the filename
-            -- -- 1: shows the relative path and shorten $HOME to ~
-            -- -- 2: shows the full path
-            -- -- 3: shows the full path and shorten $HOME to ~
-            --
-            -- Automatically updates active tab color to match color of other components (will be overidden if buffers_color is set)
-            use_mode_colors = false,
-
-            tabs_color = {
-              -- Same values as the general color option can be used here.
-              active = 'lualine_a_normal', -- Color for active tab.
-              inactive = 'lualine_b_normal', -- Color for inactive tab.
-            },
-
-            show_modified_status = false, -- Shows a symbol next to the tab name if the file has been modified.
-            symbols = {
-              modified = '', -- Text to show when the file is modified.
-            },
-
-            -- fmt = function(name, context)
-            --   -- Show + if buffer is modified in tab
-            --   local buflist = vim.fn.tabpagebuflist(context.tabnr)
-            --   local winnr = vim.fn.tabpagewinnr(context.tabnr)
-            --   local bufnr = buflist[winnr]
-            --   local mod = vim.fn.getbufvar(bufnr, '&mod')
-            --
-            --   return name .. (mod == 1 and ' +' or '')
-            -- end,
-          },
-        },
       },
     }
   end,
