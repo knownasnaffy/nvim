7f3445d1c187c9d6b371238582997140620856b9
Barinderpreet Singh <naffydharni006@gmail.com>
2025-02-07 15:44:34 +0530

    Disable workspace symbols keymap

--- lua/custom/keymaps/windows.lua
+++ lua/custom/keymaps/windows.lua
@@ -1,9 +1,8 @@
 local map = vim.keymap.set
 
---  Use CTRL+<hjkl> to switch between windows
---
 --  See `:help wincmd` for a list of all window commands
-map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+map('n', '<leader>wh', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+map('n', '<leader>wl', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+map('n', '<leader>wj', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+map('n', '<leader>wk', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+map('n', '<leader>wq', '<C-w>q', { desc = 'Close current window window' })
