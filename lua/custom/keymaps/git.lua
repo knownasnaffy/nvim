87c5f2a7f3109e215e845b2b02db13d13979b092
Barinderpreet Singh <naffydharni006@gmail.com>
2025-02-07 15:26:49 +0530

    Remove unused keymaps and change git comand labels for proper icons in where.nvim

--- lua/custom/keymaps/git.lua
+++ lua/custom/keymaps/git.lua
@@ -11,26 +11,26 @@ map('n', '<leader>gc', function()
       print 'Commit canceled.'
     end
   end)
-end, { desc = '[G]it [C]ommit with message', nowait = false })
+end, { desc = 'Git [C]ommit with message', nowait = false })
 
 map('n', '<leader>ga', function()
   vim.cmd 'G add %'
   print('File staged: ' .. vim.fn.expand '%')
-end, { desc = '[G]it [A]dd current file' })
+end, { desc = 'Git [A]dd current file' })
 
 map('n', '<leader>gs', function()
   vim.cmd 'G'
-end, { desc = '[G]it [S]tatus' })
+end, { desc = 'Git [S]tatus' })
 
 map('n', '<leader>gA', function()
   vim.cmd 'G add .'
   print 'All changes staged.'
-end, { desc = '[G]it stage [A]ll changes' })
+end, { desc = 'Git stage [A]ll changes' })
 
 map('n', '<leader>gu', function()
   vim.cmd 'G reset %'
   print('Changes unstaged: ' .. vim.fn.expand '%')
-end, { desc = '[G]it [U]nstage current file' })
+end, { desc = 'Git [U]nstage current file' })
 
 map('n', '<leader>gd', function()
   vim.ui.input({ prompt = 'Discard changes in current file? (y/n): ' }, function(input)
@@ -41,44 +41,22 @@ map('n', '<leader>gd', function()
       print 'Discard canceled.'
     end
   end)
-end, { desc = '[G]it [D]iscard changes in current file' })
+end, { desc = 'Git [D]iscard changes in current file' })
 
 map('n', '<leader>gP', function()
   vim.cmd 'G pull'
   print 'Pulled changes from remote.'
-end, { desc = '[G]it [P]ull changes from remote' })
+end, { desc = 'Git [P]ull changes from remote' })
 
 map('n', '<leader>gp', function()
   vim.cmd 'G push'
   print 'Pushed changes to remote.'
-end, { desc = '[G]it [P]ush changes to remote' })
+end, { desc = 'Git [P]ush changes to remote' })
 
 map('n', '<leader>gD', function()
   vim.cmd 'G diff %'
-end, { desc = '[G]it [D]iff current file' })
+end, { desc = 'Git [D]iff current file' })
 
 map('n', '<leader>gm', function()
   vim.cmd 'G mergetool'
-end, { desc = '[G]it [M]ergetool for conflicts' })
-
-map('n', '<leader>gco', function()
-  vim.ui.input({ prompt = 'Enter branch name to checkout: ' }, function(branch)
-    if branch then
-      vim.cmd('G checkout ' .. vim.fn.shellescape(branch))
-      print('Checked out to branch: ' .. branch)
-    else
-      print 'Checkout canceled.'
-    end
-  end)
-end, { desc = '[G]it [C]heck[O]ut branch' })
-
-map('n', '<leader>gcb', function()
-  vim.ui.input({ prompt = 'Enter new branch name: ' }, function(branch)
-    if branch then
-      vim.cmd('G checkout -b ' .. vim.fn.shellescape(branch))
-      print('Switched to new branch: ' .. branch)
-    else
-      print 'Branch creation canceled.'
-    end
-  end)
-end, { desc = '[G]it [C]reate [B]ranch' })
+end, { desc = 'Git [M]ergetool for conflicts' })
