72418fa36dbd4fbaff3cfb41a33fc0f0e9ad0f50
Barinderpreet Singh <naffydharni006@gmail.com>
2025-02-07 20:19:54 +0530

    Use defaults for statuscolumn

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -45,12 +45,6 @@ return {
     -- scroll = { enabled = true },
     statuscolumn = {
       enabled = true,
-      left = { 'mark', 'sign' }, -- priority of signs on the left (high to low)
-      right = { 'fold', 'git' }, -- priority of signs on the right (high to low)
-      folds = {
-        open = true, -- show open fold icons
-        git_hl = true, -- use Git Signs hl for fold icons
-      },
     },
     -- words = { enabled = true },
     terminal = {},
