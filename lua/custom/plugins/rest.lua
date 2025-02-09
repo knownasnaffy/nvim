248eb10b7be85efb615c27bfee1918c19e079494
Barinderpreet Singh <naffydharni006@gmail.com>
2025-02-09 12:54:57 +0530

    Configure rest.nvim to suite my workflow

--- lua/custom/plugins/rest.lua
+++ lua/custom/plugins/rest.lua
@@ -1,3 +1,50 @@
 return {
-  'rest-nvim/rest.nvim',
+  {
+    'vhyrro/luarocks.nvim',
+    branch = 'go-away-python',
+    opts = {
+      rocks = { 'lua-curl', 'nvim-nio', 'mimetypes', 'xml2lua' }, -- Specify LuaRocks packages to install
+    },
+  },
+  {
+    'rest-nvim/rest.nvim',
+    event = 'VeryLazy',
+    ft = { 'http' },
+    dependencies = {
+      {
+        'luarocks.nvim',
+      },
+    },
+    keys = {
+      {
+        '<leader>ar',
+        ':Rest run<CR>',
+      },
+      {
+        '<leader>aS',
+        ':Rest env show<CR>',
+      },
+      {
+        '<leader>as',
+        require('telescope').extensions.rest.select_env,
+      },
+    },
+    config = function()
+      vim.api.nvim_create_autocmd('BufReadPost', {
+        pattern = '*.http',
+        callback = function()
+          vim.cmd('Rest env set ' .. vim.fn.expand '%:p:h' .. '/.env')
+        end,
+      })
+
+      require 'rest-nvim'
+      ---@type rest.Opts
+      vim.g.rest_nvim = {
+        -- ...
+        request = {
+          hooks = {},
+        },
+      }
+    end,
+  },
 }
