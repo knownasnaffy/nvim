28c4b372b060d7a8c5d7388c9d536827d0eaed58
Barinderpreet Singh <naffydharni006@gmail.com>
2025-02-23 19:21:39 +0530

    Change highlight groups for undotree

--- lua/custom/plugins/undo-tree.lua
+++ lua/custom/plugins/undo-tree.lua
@@ -1,11 +1,18 @@
 return {
   'mbbill/undotree',
   config = function()
+    -- 1 = Both left; 2 = tree left, diff bottom; 3 = both right; 4 = tree right, diff bottom;
     vim.g.undotree_WindowLayout = 3
     vim.g.undotree_SplitWidth = 40
+    -- Auto focus
     vim.g.undotree_SetFocusWhenToggle = 1
     -- Disable help line
     vim.g.undotree_HelpLine = 0
+    -- Highlight groups
+    vim.g.undotree_HighlightSyntaxAdd = 'DiagnosticHint'
+    vim.g.undotree_HighlightSyntaxChange = 'DiagnosticInfo'
+    vim.g.undotree_HighlightSyntaxDel = 'DiagnosticError'
+    -- Disable statuscolumn
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'undotree',
       callback = function()
