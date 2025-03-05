655581c2f6d0bff7b411fc088894dd86d367b8e7
Barinderpreet Singh <naffydharni006@gmail.com>
2025-03-05 23:52:48 +0530

    Remove conflicts

--- lua/kickstart/plugins/auto-completion.lua
+++ lua/kickstart/plugins/auto-completion.lua
@@ -102,12 +102,12 @@ return { -- Autocompletion
         --
         -- <c-l> will move you to the right of each of the expansion locations.
         -- <c-h> is similar, except moving you backwards.
-        ['<M-;>'] = cmp.mapping(function()
+        ['<M-i>'] = cmp.mapping(function()
           if luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
           end
         end, { 'i', 's' }),
-        ['<M-j>'] = cmp.mapping(function()
+        ['<M-o>'] = cmp.mapping(function()
           if luasnip.locally_jumpable(-1) then
             luasnip.jump(-1)
           end
