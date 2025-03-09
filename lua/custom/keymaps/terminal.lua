ff45c5da9c04d7174827ae0186c161f730ddd0f2
Barinderpreet Singh <naffydharni006@gmail.com>
2025-03-09 16:00:15 +0530

    Remove unnecessary terminal keybinds

--- lua/custom/keymaps/terminal.lua
+++ lua/custom/keymaps/terminal.lua
@@ -1,17 +1,3 @@
 local map = vim.keymap.set
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
 map('t', '<M-q>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-map('t', '<M-k>', '<Down>')
-map('t', '<M-l>', '<Up>')
-map('t', '<M-j>', '<Left>')
-map('t', '<M-;>', '<Right>')
-map('t', '<M-w><M-l>', '<C-\\><C-n><C-w>k', { desc = 'Move focus to the upper window' })
-map('t', '<M-w><M-k>', '<C-\\><C-n><C-w>j', { desc = 'Move focus to the lower window' })
-map('t', '<M-w><M-;>', '<C-\\><C-n><C-w>l', { desc = 'Move focus to the right window' })
-map('t', '<M-w><M-j>', '<C-\\><C-n><C-w>h', { desc = 'Move focus to the left window' })
