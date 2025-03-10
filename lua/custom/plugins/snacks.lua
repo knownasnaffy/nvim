b472a8c827790982c2c6491b0318489bb35c3bea
Barinderpreet Singh <naffydharni006@gmail.com>
2025-03-11 00:44:49 +0530

    Remove unnecessary options (fix diagnostics bug)

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -76,18 +76,7 @@ return {
     vim.api.nvim_create_autocmd({ 'TermOpen', 'TermEnter' }, {
       pattern = '*',
       callback = function()
-        vim.diagnostic.enable(false)
         vim.wo.winbar = ''
-        vim.opt_local.number = false
-        vim.opt_local.foldcolumn = '0'
-        vim.opt_local.signcolumn = 'no'
-        vim.opt_local.cursorcolumn = false
-        vim.opt_local.wrap = false
-        vim.opt_local.scrolloff = 0
-        vim.opt_local.sidescrolloff = 0
-        vim.opt_local.cursorline = false
-        vim.opt_local.colorcolumn = ''
-        vim.opt_local.statuscolumn = ''
       end,
     })
   end,
