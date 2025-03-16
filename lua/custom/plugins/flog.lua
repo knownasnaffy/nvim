e441529e1c6008589ddb0ccebfb5a15843195233
Barinderpreet Singh <naffydharni006@gmail.com>
2025-03-16 22:48:00 +0530

    Add another awesome git plugin

---
+++ lua/custom/plugins/flog.lua
@@ -0,0 +1,11 @@
+return {
+  'rbong/vim-flog',
+  lazy = true,
+  cmd = { 'Flog', 'Flogsplit', 'Floggit' },
+  dependencies = {
+    'tpope/vim-fugitive',
+  },
+  config = function()
+    vim.g.flog_enable_extended_chars = true
+  end,
+}
