00f59119dc345c228c8b566590f122c7a79ff382
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2025-06-09 13:49:23 +0530

    Add copilot to nvim

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,11 @@
+return {
+  'github/copilot.vim',
+  keys = {
+    {
+      '<S-Space>',
+      '<Plug>(copilot-suggest)',
+      desc = 'Show copilot suggestion',
+      mode = 'i',
+    },
+  },
+}
