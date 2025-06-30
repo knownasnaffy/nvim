dbb17eb42cff2bfad7124f692949d9db597dc056
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2025-06-30 22:26:21 +0530

    Use new path for mason nvim-lua/kickstart.nvim#1516

--- lua/kickstart/plugins/lsp.lua
+++ lua/kickstart/plugins/lsp.lua
@@ -3,8 +3,8 @@ return {
   'neovim/nvim-lspconfig',
   dependencies = {
     -- Automatically install LSPs and related tools to stdpath for Neovim
-    { 'williamboman/mason.nvim', opts = {} }, -- NOTE: Must be loaded before dependants
-    'williamboman/mason-lspconfig.nvim',
+    { 'mason-org/mason.nvim', opts = {} }, -- NOTE: Must be loaded before dependants
+    'mason-org/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
 
     -- Useful status updates for LSP. (Unused, using lsp-notify instead)
