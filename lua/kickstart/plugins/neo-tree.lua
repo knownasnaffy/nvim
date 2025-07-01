28c6f6ed100b2737dd766b42a7f0ad02b4e4b390
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2025-07-02 00:30:09 +0530

    Remove lazy loading for neo-tree nvim-lua/kickstart.nvim#1489

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -12,7 +12,7 @@ return {
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  cmd = 'Neotree',
+  lazy = false,
   keys = {
     { '<M-e>', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
     { '<leader>e', ':Neotree reveal<CR>', desc = 'Open [E]xplorer', silent = true },
