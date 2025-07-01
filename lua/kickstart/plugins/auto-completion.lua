61071f6bb0848d27bae54c30d110a1b7c45764e2
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2025-07-01 23:44:44 +0530

    Add emoji completion to markdown and gitcommit buffers

--- lua/kickstart/plugins/auto-completion.lua
+++ lua/kickstart/plugins/auto-completion.lua
@@ -28,6 +28,7 @@ return { -- Autocompletion
       },
     },
     'folke/lazydev.nvim',
+    'moyiz/blink-emoji.nvim',
   },
   --- @module 'blink.cmp'Add commentMore actions
   --- @type blink.cmp.Config
@@ -106,9 +107,29 @@ return { -- Autocompletion
     },
 
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'lazydev' },
+      default = { 'lsp', 'path', 'snippets', 'lazydev', 'emoji' },
       providers = {
         lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+        emoji = {
+          module = 'blink-emoji',
+          name = 'Emoji',
+          score_offset = 15, -- Tune by preference
+          opts = {
+            insert = true, -- Insert emoji (default) or complete its name
+            ---@type string|table|fun():table
+            trigger = function()
+              return { ':' }
+            end,
+          },
+          should_show_items = function()
+            return vim.tbl_contains(
+              -- Enable emoji completion only for git commits and markdown.
+              -- By default, enabled for all file-types.
+              { 'gitcommit', 'markdown' },
+              vim.o.filetype
+            )
+          end,
+        },
       },
     },
 
