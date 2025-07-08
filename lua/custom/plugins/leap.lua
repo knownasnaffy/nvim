bdb1fec802a8fc29d8ebff95f756280cae5f12db
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2025-07-09 03:53:29 +0530

    Use F keybind for leap as f is useful in macros

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -1,6 +1,6 @@
 return {
   'ggandor/leap.nvim',
   config = function()
-    vim.keymap.set({ 'n', 'x', 'o' }, 'f', '<Plug>(leap)')
+    vim.keymap.set({ 'n', 'x', 'o' }, 'F', '<Plug>(leap)')
   end,
 }
