86cedffc9cda399b65b64ee8d854398101fe1d9f
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2025-07-15 16:16:32 +0530

    Use latest neorg version

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -2,7 +2,6 @@ return {
   'nvim-neorg/neorg',
   lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
   -- enabled = false,
-  version = '*', -- Pin Neorg to the latest stable release
   dependencies = {
     { 'nvim-lua/plenary.nvim' },
     { 'nvim-neorg/neorg-telescope' },
