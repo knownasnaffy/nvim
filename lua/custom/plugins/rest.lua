1df0f310cd6a49b12a98414e1fd506e71d37940f
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2025-08-12 09:43:43 +0530

    Update rest client plugin

--- lua/custom/plugins/rest.lua
+++ lua/custom/plugins/rest.lua
@@ -1,72 +1,14 @@
 return {
-  {
-    'vhyrro/luarocks.nvim',
-    branch = 'go-away-python',
+  'mistweaverco/kulala.nvim',
+  keys = {
+    { '<leader>Rs', desc = 'Send request' },
+    { '<leader>Ra', desc = 'Send all requests' },
+    { '<leader>Rb', desc = 'Open scratchpad' },
   },
-  {
-    'rest-nvim/rest.nvim',
-    event = 'VeryLazy',
-    ft = { 'http' },
-    dependencies = {
-      {
-        'luarocks.nvim',
-      },
-    },
-    keys = {
-      {
-        '<leader>ar',
-        ':Rest run<CR>',
-      },
-      {
-        '<leader>aS',
-        ':Rest env show<CR>',
-      },
-      {
-        '<leader>as',
-        ':Rest env select<CR>',
-      },
-      -- {
-      --   '<leader>af',
-      --   '<C-w>l:set ma<CR>/@_RES<CR>:nohlsearch<CR>jV:!jq<CR><C-w>h',
-      --   desc = 'Rest json format',
-      -- },
-    },
-    config = function()
-      vim.api.nvim_create_autocmd('BufReadPost', {
-        pattern = '*.http',
-        callback = function()
-          local dir = vim.fn.expand '%:p:h' -- Get directory of the opened file
-
-          while dir and dir ~= '/' do
-            local env_path = dir .. '/.env'
-            if vim.fn.filereadable(env_path) == 1 then
-              vim.cmd('Rest env set ' .. env_path)
-              return -- Stop searching once we find the .env file
-            end
-            dir = vim.fn.fnamemodify(dir, ':h') -- Move up one directory
-          end
-        end,
-      })
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = 'json',
-        callback = function(ev)
-          vim.bo[ev.buf].formatprg = 'jq'
-        end,
-      })
-
-      require 'rest-nvim'
-      ---@type rest.Opts
-      vim.g.rest_nvim = {
-        request = {
-          hooks = {},
-        },
-        ui = {
-          keybinds = {
-            next = 'O',
-            prev = 'I',
-          },
-        },
-      }
-    end,
+  ft = { 'http', 'rest' },
+  opts = {
+    global_keymaps = true,
+    global_keymaps_prefix = '<leader>R',
+    kulala_keymaps_prefix = '',
   },
 }
