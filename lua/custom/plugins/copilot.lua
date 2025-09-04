9ad08e54a4db3a75737d25635cf11724e55d2aaa
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2025-09-05 03:05:47 +0530

    Disable copilot for .env files and react typescript

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -5,7 +5,18 @@ return {
   config = function()
     local copilot = require 'copilot'
 
-    copilot.setup {}
+    copilot.setup {
+      fyletypes = {
+        sh = function()
+          if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+            -- disable for .env files
+            return false
+          end
+          return true
+        end,
+        typescriptreact = false,
+      },
+    }
 
     -- add some default keybinds
   end,
