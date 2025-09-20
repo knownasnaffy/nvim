968ae6a2bfa91f6e802f8204675067683443a316
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2025-09-20 06:54:16 +0530

    Add keymap for jumping between terminals

--- lua/custom/keymaps/terminal.lua
+++ lua/custom/keymaps/terminal.lua
@@ -1,3 +1,5 @@
 local map = vim.keymap.set
 
 map('t', '<M-q>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+map('t', '<M-,>', '<Cmd>1ToggleTerm<CR>', { desc = 'Go to terminal 1' })
+map('t', '<M-.>', '<Cmd>2ToggleTerm<CR>', { desc = 'Go to terminal 2' })
