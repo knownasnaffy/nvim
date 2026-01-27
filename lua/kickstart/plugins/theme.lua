82957e7ba92ef1786279bd38ec3d4202fa873bf4
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2026-01-28 02:10:45 +0530

    Remove cursorline with shader

--- lua/kickstart/plugins/theme.lua
+++ lua/kickstart/plugins/theme.lua
@@ -18,6 +18,7 @@ return {
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-night'
+      vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#1a1b26' })
     end,
   },
 }
