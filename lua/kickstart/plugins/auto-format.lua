0501c713d03bdc350c7d1688029b4876bf085e67
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2026-02-08 16:47:14 +0530

    Make bash format use stdin/stdout instead of in-place file editing

--- lua/kickstart/plugins/auto-format.lua
+++ lua/kickstart/plugins/auto-format.lua
@@ -98,8 +98,9 @@ return { -- Autoformat
             return {
               exe = 'beautysh',
               args = {
-                util.escape_path(util.get_current_buffer_file_path()),
+                '-',
               },
+              stdin = true,
             }
           end,
         },
