cc418f92d20501eba37738dad25e31bd2ec0be6f
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2026-02-09 22:13:45 +0530

    Remove cursor hide option as it had no effect

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -49,14 +49,6 @@ return {
     },
     event_handlers = {
       {
-        event = 'neo_tree_buffer_enter',
-        handler = function() vim.cmd 'highlight! Cursor blend=100' end,
-      },
-      {
-        event = 'neo_tree_buffer_leave',
-        handler = function() vim.cmd 'highlight! Cursor guibg=#5f87af blend=0' end,
-      },
-      {
         event = 'file_renamed',
         handler = on_move,
       },
