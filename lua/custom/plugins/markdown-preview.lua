891e945847788b8c8d5e5d7230bc11cc63f4f211
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2026-02-14 16:38:44 +0530

    Add open markdown preview keybind

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -4,4 +4,12 @@ return {
   build = 'cd app && bun install',
   init = function() vim.g.mkdp_filetypes = { 'markdown' } end,
   ft = { 'markdown' },
+  keys = {
+
+    {
+      '<leader>oM',
+      '<Cmd>MarkdownPreviewToggle<CR>',
+      desc = 'Hover [O]ver the [I]mage the cursor is at',
+    },
+  },
 }
