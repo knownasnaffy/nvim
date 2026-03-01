88bd3ab3f766f947fea9bfaa580289b37bcbda56
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2026-03-02 03:52:58 +0530

    Replace formatter plugin

--- lua/kickstart/plugins/auto-format.lua
+++ lua/kickstart/plugins/auto-format.lua
@@ -1,145 +1,54 @@
-return { -- Autoformat
-  'mhartington/formatter.nvim',
+return {
+  'stevearc/conform.nvim',
+  keys = {
+    {
+      '<M-s>',
+      '<Cmd>w<CR>',
+      desc = 'Save buffer',
+    },
+  },
   config = function()
-    -- Utilities for creating configurations
-    local util = require 'formatter.util'
+    local conform = require 'conform'
 
-    local function jsonParser()
-      return {
-        exe = 'prettier',
-        args = { '--parser', 'json' },
-        stdin = true,
-      }
-    end
-    -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-    require('formatter').setup {
-      -- Enable or disable logging
-      logging = true,
-      -- Set the log level
-      log_level = vim.log.levels.WARN,
-      -- All formatter configurations are opt-in
-      filetype = {
-        -- Formatter configurations for filetype "lua" go here
-        -- and will be executed in order
+    conform.setup {
+      formatters_by_ft = {
+        lua = { 'stylua' },
 
-        lua = {
-          -- "formatter.filetypes.lua" defines default configurations for the
-          -- "lua" filetype
-          require('formatter.filetypes.lua').stylua,
+        javascript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
 
-          -- You can also define your own configuration
-          -- function()
-          --   -- Supports conditional formatting
-          --   if util.get_current_buffer_file_name() == 'special.lua' then
-          --     return nil
-          --   end
-          --
-          --   -- Full specification of configurations is down below and in Vim help
-          --   -- files
-          --   return {
-          --     exe = 'stylua',
-          --     args = {
-          --       '--search-parent-directories',
-          --       '--stdin-filepath',
-          --       util.escape_path(util.get_current_buffer_file_path()),
-          --       '--',
-          --       '-',
-          --     },
-          --     stdin = true,
-          --   }
-          -- end,
-        },
+        json = { 'prettier' },
+        jsonc = { 'prettier' },
 
-        javascriptreact = {
-          require 'formatter.defaults.prettier',
-        },
-        javascript = {
-          require 'formatter.defaults.prettier',
-        },
-        typescript = {
-          require 'formatter.defaults.prettier',
-        },
-        typescriptreact = {
-          require 'formatter.defaults.prettier',
-        },
-        json = {
-          jsonParser,
-        },
-        jsonc = {
-          jsonParser,
-        },
-        css = {
-          require('formatter.filetypes.css').prettier,
-        },
-        html = {
-          require('formatter.filetypes.html').prettier,
-        },
-        toml = {
-          require('formatter.filetypes.toml').taplo,
-        },
-        yaml = {
-          require('formatter.filetypes.yaml').prettier,
-        },
-        python = {
-          require('formatter.filetypes.python').black,
-        },
-        dart = {
-          require('formatter.filetypes.dart').dartformat,
-        },
-        cpp = {
-          require('formatter.filetypes.cpp').clangformat,
-        },
-        sh = {
-          function()
-            -- Supports conditional formatting
+        css = { 'prettier' },
+        html = { 'prettier' },
+        yaml = { 'prettier' },
 
-            -- Full specification of configurations is down below and in Vim help
-            -- files
-            return {
-              exe = 'beautysh',
-              args = {
-                '-',
-              },
-              stdin = true,
-            }
-          end,
-        },
-        qml = {
-          function()
-            -- Supports conditional formatting
+        toml = { 'taplo' },
 
-            -- Full specification of configurations is down below and in Vim help
-            -- files
-            return {
-              exe = 'qmlformat',
-              args = {
-                '-i',
-                util.escape_path(util.get_current_buffer_file_path()),
-              },
-            }
-          end,
-        },
-        go = {
-          require('formatter.filetypes.go').gofmt,
-        },
-        rust = {
-          require('formatter.filetypes.rust').rustfmt(),
-        },
-        xml = {
-          require('formatter.filetypes.xml').xmlformat,
-        },
-        -- Use the special "*" filetype for defining formatter configurations on
-        -- any filetype
-        ['*'] = {
-          -- "formatter.filetypes.any" defines default configurations for any
-          -- filetype
-          require('formatter.filetypes.any').remove_trailing_whitespace,
-          -- Remove trailing whitespace without 'sed'
-          -- require("formatter.filetypes.any").substitute_trailing_whitespace,
-        },
+        python = { 'black' },
+
+        dart = { 'dart_format' },
+
+        cpp = { 'clang_format' },
+
+        sh = { 'beautysh' },
+
+        qml = { 'qmlformat' },
+
+        go = { 'gofmt' },
+
+        rust = { 'rustfmt' },
+
+        xml = { 'xmlformat' },
+      },
+
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = 'fallback',
       },
     }
-    vim.keymap.set('n', '<M-s>', '<Cmd>w | FormatWrite<CR>', { desc = '[F]ormat buffer' })
-    vim.keymap.set('n', '<M-S>', '<Cmd>w<CR>', { desc = '[S]ave buffer' })
   end,
 }
