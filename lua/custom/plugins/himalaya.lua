7b1bdbe38a04fdc8a8ed3413c18d3cb10f27ccdb
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2026-03-01 21:33:14 +0530

    Update himalaya conf

--- lua/custom/plugins/himalaya.lua
+++ lua/custom/plugins/himalaya.lua
@@ -1,8 +1,33 @@
 return {
-  'himalaya-vim',
+  'himalaya.nvim',
+  dependencies = { 'MunifTanjim/nui.nvim' },
   dev = true,
+  -- Disable lazy loading if you want to use Himalaya when vim starts, i.e, `nvim +':Himalaya'`
   lazy = false,
+  opts = {
+    icons_enabled = true,
+    wrap_folder_navigation = false, -- wrap to first/last when navigating
+    keymaps = {
+      listing = {
+        ['s'] = 'folder_picker',
+        ['r'] = function() require('himalaya.folder').reload() end,
+        ['gr'] = 'none',
+      },
+    },
+  },
   keys = {
     { '<leader>oh', '<Cmd>Himalaya<CR>', desc = '[O]pen [H]imalaya' },
   },
 }
+
+-- return {
+--   'knownasnaffy/himalaya.nvim',
+--   dependencies = { 'MunifTanjim/nui.nvim' },
+--   opts = {
+--     icons_enabled = true, -- enable nerd font icons
+--   },
+--   keys = {
+--     { '<leader>oh', '<Cmd>Himalaya<CR>', desc = '[O]pen [H]imalaya' },
+--   },
+-- }
+--
