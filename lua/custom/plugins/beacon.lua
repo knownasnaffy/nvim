8512ba1aace6197a7d85e02800c34bcb0cb3de10
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2026-03-22 11:35:19 +0530

    Remove beacon

--- lua/custom/plugins/beacon.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'danilamihailov/beacon.nvim',
-  opts = {
-    window_events = { 'WinEnter' }, -- table<string> what events trigger cursor highlight
-  },
-} -- Cursor highlight after motion
